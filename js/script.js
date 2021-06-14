da1313365434dd9f4753ba2c51def20ea2dee853
Renanda Adrian Putra <renanda1301@gmail.com>
2021-06-15 00:45:36 +0800

    add button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-$(function () {});
+// $(function () {
+//   let menuTitle = $("#dropdown-content1").prev();
+//   menuTitle.on("mouseenter", () => {
+//     menuTitle.css({
+//       "bottom-border": "3px solid #ffd60a",
+//     });
+//   });
+//   console.log(menuTitle);
+//   console.log("test");
+// });
