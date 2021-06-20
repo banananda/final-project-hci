356d790986eb12489efb09beef3d4d9e0797b294
Renanda Adrian Putra <renanda1301@gmail.com>
2021-06-20 15:57:36 +0800

    navbar update

--- js/script.js
+++ js/script.js
@@ -1,90 +1,94 @@
 $(function () {
-  let toggleState = false; //initial state of main dropdown menu
-  let toggleStateInfo = false;
-  let toggleStateSocials = false;
+  let screenWidth = window.innerWidth;
+  console.log(screenWidth);
+  if (screenWidth <= 800) {
+    let toggleState = false; //initial state of main dropdown menu
+    let toggleStateInfo = false;
+    let toggleStateSocials = false;
 
-  let dropdownInfo = $("#dropdown-info");
-  let menuTitleInfo = $("#menu-title-info");
+    let dropdownInfo = $("#dropdown-info");
+    let menuTitleInfo = $("#menu-title-info");
 
-  let dropdownSocials = $("#dropdown-socials");
-  let menuTitleSocials = $("#menu-title-socials");
-  toggleDropdown = () => {
-    let dropdownMenu = $("#menu");
-    let preRegButton = $("#pre-reg-btn");
-    if (toggleState == false) {
-      dropdownMenu.css({
-        display: "flex",
-        transition: "all 500ms",
-      });
-      preRegButton.css({
-        display: "block",
-      });
-      toggleState = true;
-    } else {
-      dropdownMenu.css({
-        display: "none",
-        transition: "all 500ms",
-      });
-      preRegButton.css({
-        display: "none",
-      });
-      toggleState = false;
-    }
-  };
+    let dropdownSocials = $("#dropdown-socials");
+    let menuTitleSocials = $("#menu-title-socials");
+    toggleDropdown = () => {
+      let dropdownMenu = $("#menu");
+      let preRegButton = $("#pre-reg-btn");
+      if (toggleState == false) {
+        dropdownMenu.css({
+          display: "flex",
+          transition: "all 500ms",
+        });
+        preRegButton.css({
+          display: "block",
+        });
+        toggleState = true;
+      } else {
+        dropdownMenu.css({
+          display: "none",
+          transition: "all 500ms",
+        });
+        preRegButton.css({
+          display: "none",
+        });
+        toggleState = false;
+      }
+    };
 
-  toggleInfo = () => {
-    if (toggleStateInfo == false) {
-      dropdownInfo.css({
-        display: "block",
-      });
-      menuTitleInfo.css({
-        "border-bottom": "2px solid #ffd60a",
-      });
-      toggleStateInfo = true;
+    toggleInfo = () => {
+      if (toggleStateInfo == false) {
+        dropdownInfo.css({
+          display: "block",
+        });
+        menuTitleInfo.css({
+          "border-bottom": "2px solid #ffd60a",
+        });
+        toggleStateInfo = true;
 
-      dropdownSocials.css({
-        display: "none",
-      });
-      menuTitleSocials.css({
-        "border-bottom": "1px solid #001226",
-      });
-      toggleStateSocials = false;
-    } else {
-      dropdownInfo.css({
-        display: "none",
-      });
-      menuTitleInfo.css({
-        "border-bottom": "1px solid #001226",
-      });
-      toggleStateInfo = false;
-    }
-  };
+        dropdownSocials.css({
+          display: "none",
+        });
+        menuTitleSocials.css({
+          "border-bottom": "1px solid #001226",
+        });
+        toggleStateSocials = false;
+      } else {
+        dropdownInfo.css({
+          display: "none",
+        });
+        menuTitleInfo.css({
+          "border-bottom": "1px solid #001226",
+        });
+        toggleStateInfo = false;
+      }
+    };
 
-  toggleSocials = () => {
-    if (toggleStateSocials == false) {
-      dropdownSocials.css({
-        display: "block",
-      });
-      menuTitleSocials.css({
-        "border-bottom": "2px solid #ffd60a",
-      });
-      toggleStateSocials = true;
+    toggleSocials = () => {
+      if (toggleStateSocials == false) {
+        dropdownSocials.css({
+          display: "block",
+        });
+        menuTitleSocials.css({
+          "border-bottom": "2px solid #ffd60a",
+        });
+        toggleStateSocials = true;
 
-      dropdownInfo.css({
-        display: "none",
-      });
-      menuTitleInfo.css({
-        "border-bottom": "1px solid #001226",
-      });
-      toggleStateInfo = false;
-    } else {
-      dropdownSocials.css({
-        display: "none",
-      });
-      menuTitleSocials.css({
-        "border-bottom": "1px solid #001226",
-      });
-      toggleStateSocials = false;
-    }
-  };
+        dropdownInfo.css({
+          display: "none",
+        });
+        menuTitleInfo.css({
+          "border-bottom": "1px solid #001226",
+        });
+        toggleStateInfo = false;
+      } else {
+        dropdownSocials.css({
+          display: "none",
+        });
+        menuTitleSocials.css({
+          "border-bottom": "1px solid #001226",
+        });
+        toggleStateSocials = false;
+      }
+    };
+  }
 });
